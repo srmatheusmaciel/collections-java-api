2c771085024cc2861c639265168b08ba0c262381
srmatheusmaciel <srmatheusmaciel@hotmail.com>
2024-09-23 21:19:32 -0300

    Criei uma List para adcionar itens ao carrinho de compra.

---
+++ src/main/list/OperacoesBasicas/Item.java
@@ -0,0 +1,40 @@
+package main.list.OperacoesBasicas;
+
+public class Item {
+
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Item(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
---
+++ src/main/list/OperacoesBasicas/ListaTarefas.java
@@ -0,0 +1,58 @@
+package main.list.OperacoesBasicas;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefas() {
+        this.tarefaList = new ArrayList<>();
+    };
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+
+        for(Tarefa t : tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+
+                tarefasParaRemover.add(t);
+
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalTarefas(){
+
+        return tarefaList.size();
+
+    }
+
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+       ListaTarefas listaTarefa = new ListaTarefas();
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Primeira Tarefa é adcionar a primeira tarefa");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        listaTarefa.adicionarTarefa("Tarefa 3");
+
+        listaTarefa.removerTarefa("Tarefa 2");
+
+
+
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+        listaTarefa.obterDescricoesTarefas();
+    }
+}
